7c5e159c648136bee507064cce164c61e8419d5b
Julia Kocur <juliakocur@mail.com>
2023-04-04 12:24:46 +0200

    docs: create slider for the main page

--- shelter/pets/index.js
+++ shelter/pets/index.js
@@ -1,1 +1,31 @@
 console.log(`Оценка за задание 100/100\n Main\n Проверка верстки +7\n Вёрстка соответствует макету +35\n Требования к css +6 \n Интерактивность элементов +12 \n Pets\n Проверка верстки +7\n Вёрстка соответствует макету +15\n Требования к css +4\n Интерактивность элементов +14`) 
+
+const iconMenu = document.querySelector('.burger');
+const navMenu = document.querySelector('.menu-list');
+
+
+
+    iconMenu.addEventListener("click", () => {
+    document.body.classList.toggle('_lock');
+    document.querySelector('.shadow').classList.toggle('_active');
+    iconMenu.classList.toggle('_active');
+    navMenu.classList.toggle('_active');
+    });
+
+
+/* закрытие бургер меню */
+const closeBurger = () => {
+        document.body.classList.remove('_lock');
+        document.querySelector('.shadow').classList.remove('_active');
+        iconMenu.classList.remove('_active');
+        navMenu.classList.remove('_active');
+};
+
+navMenu.addEventListener("click", () => {
+    closeBurger();
+});
+
+document.querySelector('.shadow').addEventListener("click", () => {
+  if (navMenu.classList.contains('_active')) closeBurger();
+});
+
