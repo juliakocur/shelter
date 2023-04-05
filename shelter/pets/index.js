8fa717f5618f2c44609f06e2621c71b0f5203715
Julia Kocur <116370785+juliakocur@users.noreply.github.com>
2023-04-05 10:36:23 +0200

    Revert "Shelter"

--- shelter/pets/index.js
+++ shelter/pets/index.js
@@ -5,46 +5,21 @@ const navMenu = document.querySelector('.menu-list');
 
 
 
-
-    iconMenu.addEventListener("click", () => {
-
 if (iconMenu) {
     iconMenu.addEventListener("click", function (e) {
-
     document.body.classList.toggle('_lock');
     document.querySelector('.shadow').classList.toggle('_active');
     iconMenu.classList.toggle('_active');
     navMenu.classList.toggle('_active');
-
-    });
-
-
-/* закрытие бургер меню */
-const closeBurger = () => {
-=======
 });
 }
 
 /* закрытие бургер меню */
 if (navMenu) {
         navMenu.addEventListener("click", function (e) {
-
         document.body.classList.remove('_lock');
         document.querySelector('.shadow').classList.remove('_active');
         iconMenu.classList.remove('_active');
         navMenu.classList.remove('_active');
-
-};
-
-navMenu.addEventListener("click", () => {
-    closeBurger();
-});
-
-document.querySelector('.shadow').addEventListener("click", () => {
-  if (navMenu.classList.contains('_active')) closeBurger();
-});
-
-
 });
 }
-
