bef5e061970d1592a713c26a9587a04f4df9465b
Julia Kocur <116370785+juliakocur@users.noreply.github.com>
2023-04-05 11:56:12 +0200

    docs: update pets index.js

--- shelter/pets/index.js
+++ shelter/pets/index.js
@@ -5,35 +5,20 @@ const navMenu = document.querySelector('.menu-list');
 
 
 
-
     iconMenu.addEventListener("click", () => {
-
-if (iconMenu) {
-    iconMenu.addEventListener("click", function (e) {
-
     document.body.classList.toggle('_lock');
     document.querySelector('.shadow').classList.toggle('_active');
     iconMenu.classList.toggle('_active');
     navMenu.classList.toggle('_active');
-
     });
 
 
 /* закрытие бургер меню */
 const closeBurger = () => {
-=======
-});
-}
-
-/* закрытие бургер меню */
-if (navMenu) {
-        navMenu.addEventListener("click", function (e) {
-
         document.body.classList.remove('_lock');
         document.querySelector('.shadow').classList.remove('_active');
         iconMenu.classList.remove('_active');
         navMenu.classList.remove('_active');
-
 };
 
 navMenu.addEventListener("click", () => {
@@ -43,8 +28,4 @@ navMenu.addEventListener("click", () => {
 document.querySelector('.shadow').addEventListener("click", () => {
   if (navMenu.classList.contains('_active')) closeBurger();
 });
-
-
-});
-}
 
